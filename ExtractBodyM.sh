cf8e94e2017fcd12ed744a8308382020479b864d
Omer Lavian <omerlavian20@g.ucla.edu>
2020-01-25 21:45:04 -0800

    added all the script text with nano open

--- ExtractBodyM.sh
+++ ExtractBodyM.sh
@@ -1,1 +1,8 @@
+tail -n +2 Pacifici2013_data.csv | cut -d ";" -f 2-6 | tr ";" " " | sort -r -n -k 6 > bodyM.csv
+
+ # skip first line of file # restict extraction to columns 2-6 
+# replace semicolons with spaces 
+# sort organisms by decreasing bodymass 
+# place the results into a file called bodyM.csv
+#To use the command without typing "bash", move the script to /usrbin or /bin
 
