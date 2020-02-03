887e75864b996c3b49e1c325d98976481ed105fd
Omer Lavian <omerlavian20@g.ucla.edu>
2020-02-03 15:35:15 -0800

    generalized ExtraxtBodyM.sh

--- ExtractBodyM.sh
+++ ExtractBodyM.sh
@@ -1,6 +1,8 @@
-tail -n +2 Pacifici2013_data.csv | cut -d ";" -f 2-6 | tr ";" " " | sort -r -n -k 6 > bodyM.csv
+INPUT=$1
+OUTPUT=$2
+tail -n +2 $INPUT | cut -d ";" -f 2-6 | tr ";" " " | sort -r -n -k 6 > $OUTPUT
 
- # skip first line of file # restict extraction to columns 2-6 
+# skip first line of file # restict extraction to columns 2-6 
 # replace semicolons with spaces 
 # sort organisms by decreasing bodymass 
 # place the results into a file called bodyM.csv
