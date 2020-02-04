2dee629de719c6dcc952a25b4127d27bab72dec7
Omer Lavian <omerlavian20@g.ucla.edu>
2020-02-03 20:05:37 -0800

    modified extraction script

--- ExtractBodyM.sh
+++ ExtractBodyM.sh
@@ -1,10 +1,14 @@
+#!/bin/bash
 INPUT=$1
-OUTPUT=$2
-tail -n +2 $INPUT | cut -d ";" -f 2-6 | tr ";" " " | sort -r -n -k 6 > $OUTPUT
+DELIM=$2
+OUTPUT=$3
+
+tail -n +2 $INPUT|cut -d $DELIM -f 2-6|tr $DELIM " "|sort -r -n -k 6 >> $OUTPUT
 
 # skip first line of file # restict extraction to columns 2-6 
 # replace semicolons with spaces 
 # sort organisms by decreasing bodymass 
-# place the results into a file called bodyM.csv
+# place the results into a file of any name
 #To use the command without typing "bash", move the script to /usrbin or /bin
+
 
